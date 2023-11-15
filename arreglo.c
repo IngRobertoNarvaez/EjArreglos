2c06ad4ca43cc3a0b07dd67fb6d99a3467970620
Roberto Narvaez <roberto.narvaez@udla.edu.ec>
2023-11-15 15:34:04 -0500

    Uso de semilla y nuevas librerias

--- arreglo.c
+++ arreglo.c
@@ -1,27 +1,32 @@
 #include <stdio.h>
+// nuevas librerias
+#include <stdlib.h>
+#include <time.h>
 int main(int argc, char const *argv[])
 {
-    /*int vector1[10];
+    // semilla
+    srand(time(NULL));
+    int vector1[10];
     // Se mostrata los vetores
     // Se trabaja con for para usar el i (indice) para que nos pueda mostrar al final el resultado
     for (size_t i = 0; i < 10; i++)
     {
-        printf("Ingrese el contenido del vector : \n ");
-        scanf("%d", &vector1[i]);
-    }*/
+        vector1[i] = rand() % 10;
+        /*printf("Ingrese el contenido del vector : \n ");
+        scanf("%d", &vector1[i]);*/
+    }
     // Se mete otro for para la imprencion del resultado usando el indice
+    for (size_t i = 0; i < 10; i++)
+    {
 
-    char vector2[10] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'};
+        printf("%d ", vector1[i]);
+    }
+    /*char vector2[10] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'};
 
-    /* for (size_t i = 0; i < 10; i++)
-          {
-
-              printf("%d ", vector1[i]);
-          }*/
     for (size_t i = 0; i < 10; i++)
     {
 
         printf("%c ", vector2[i]);
-    }
+    }*/
     return 0;
 }
