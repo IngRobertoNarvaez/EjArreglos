7e1e34eb1a6a73fc21eb861220e9995dc5ae8a82
Roberto Narvaez <roberto.narvaez@udla.edu.ec>
2023-11-15 15:24:20 -0500

    Vectores con caracteres letras

--- arreglo.c
+++ arreglo.c
@@ -1,19 +1,27 @@
 #include <stdio.h>
 int main(int argc, char const *argv[])
 {
-    int vector1[10];
+    /*int vector1[10];
     // Se mostrata los vetores
     // Se trabaja con for para usar el i (indice) para que nos pueda mostrar al final el resultado
     for (size_t i = 0; i < 10; i++)
     {
         printf("Ingrese el contenido del vector : \n ");
         scanf("%d", &vector1[i]);
-    }
+    }*/
     // Se mete otro for para la imprencion del resultado usando el indice
+
+    char vector2[10] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j'};
+
+    /* for (size_t i = 0; i < 10; i++)
+          {
+
+              printf("%d ", vector1[i]);
+          }*/
     for (size_t i = 0; i < 10; i++)
     {
 
-        printf("%d ", vector1[i]);
+        printf("%c ", vector2[i]);
     }
     return 0;
 }
