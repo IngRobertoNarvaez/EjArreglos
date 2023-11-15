a6fc2ba6efd168b3ad362fd7772e6a22bed3e0e2
Roberto Narvaez <roberto.narvaez@udla.edu.ec>
2023-11-15 15:15:38 -0500

    Imprime los vectores

--- arreglo.c
+++ arreglo.c
@@ -2,6 +2,18 @@
 int main(int argc, char const *argv[])
 {
     int vector1[10];
+    // Se mostrata los vetores
+    // Se trabaja con for para usar el i (indice) para que nos pueda mostrar al final el resultado
+    for (size_t i = 0; i < 10; i++)
+    {
+        printf("Ingrese el contenido del vector : \n ");
+        scanf("%d", &vector1[i]);
+    }
+    // Se mete otro for para la imprencion del resultado usando el indice
+    for (size_t i = 0; i < 10; i++)
+    {
 
+        printf("-%d", vector1[i]);
+    }
     return 0;
 }
